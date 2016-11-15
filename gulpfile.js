844f7dbb6cc8a2798c9721d6f87c1cf4dc14837e
brain <brain00021@gmail.com>
2016-11-15 10:59:01 +0800

    gulp_20161114

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,37 @@ var gulp = require('gulp'), // 載入 gulp
     gulpPlumber = require('gulp-plumber'), // 載入 gulp-plumber
     autoprefixer = require('gulp-autoprefixer'), //瀏覽器的前置號
     sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create();
-// var notify = require("gulp-notify");
-var sassLint = require('gulp-sass-lint');//他說檢查錯誤 誰知道
+    gulpImagemin = require('gulp-imagemin'),
+    browserSync = require('browser-sync').create(),
+    gulpLivereload = require('gulp-livereload'),  // 載入 gulp-livereload
+	gulpNotify = require("gulp-notify");
+
+var sassLint = require('gulp-sass-lint');//他說檢查錯誤 
 // var postcss      = require('gulp-postcss');
 
 
 
 gulp.task('watch', function() {
+	gulpLivereload.listen();
     gulp.watch('js/*.js', ['scripts']);
     gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('script', function() {
     gulp.src('js/*.js') // 指定要處理的原始 JavaScript 檔案目錄
-        .pipe(gulpPlumber()) //判斷除錯
+        .pipe(gulpPlumber()) //判斷除錯不要一直斷watch
         // .on('error', console.error.bind(console)) // 使用錯誤事件處理例外
         .pipe(gulpUglify()) // 將 JavaScript 做最小化
-        .pipe(gulp.dest('dist')); // 指定最小化後的 JavaScript 檔案目錄
+        .pipe(gulp.dest('dist')) // 指定最小化後的 JavaScript 檔案目錄
+        .pipe(gulpLivereload())                // 當檔案異動後自動重新載入頁面
+        .pipe(gulpNotify("Minify JavaScript Finish"));
+});
+
+gulp.task('image', function () {
+    gulp.src('images/original/**') //指定的路徑
+        .pipe(gulpImagemin()) // 縮編
+        .pipe(gulp.dest('images')); //匯出檔案
 });
 // 自我練習的task
 
@@ -33,6 +46,13 @@ gulp.task('script', function() {
 //         .pipe(gulp.dest('css'));  // 指定編譯後的 css 檔案目錄
 // });
 
+// 服務
+gulp.task('serve', ['sass'], function () {
+    browserSync.init({
+        proxy: "kejyun.dev"   // hostname
+    });
+});
+
 // sass task其他人給我的task
 gulp.task('styles', function() {
     return gulp.src('sass/**/*.+(scss|sass)')
@@ -41,12 +61,14 @@ gulp.task('styles', function() {
         .pipe(gulpSass({ outputStyle: 'expanded' }).on('error', gulpSass.logError))
         // .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
         // .pipe(autoprefixer({ browsers: ['> 5%, last 2 versions, ff 17, opera 12.1'] }))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({ browsers: ['> 5%','ie 6-8','Firefox <= 20'] }))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }));
+        }))
+        .pipe(browserSync.stream()) //同步
+        .pipe(gulpLivereload());                // 當檔案異動後自動重新載入頁面
 });
 
 // compass task作法
